307c5b238c83c8cfc1c5951a099ee2a06867021d
Sebastian Veit <sebastian.veit@lucimber.com>
2025-06-15 15:11:53 +0200

    core: Remove PLATFORM_WINDOWS condition from rtw_pwrctrl.c (#94)
    Signed-off-by: Sebastian Veit <sebastian.veit@lucimber.com>

--- core/rtw_pwrctrl.c
+++ core/rtw_pwrctrl.c
@@ -2100,9 +2100,6 @@ void rtw_init_pwrctrl_priv(PADAPTER padapter)
 #endif
 
 
-#ifdef PLATFORM_WINDOWS
-	pwrctrlpriv->pnp_current_pwr_state = NdisDeviceStateD0;
-#endif
 
 	_init_pwrlock(&pwrctrlpriv->lock);
 	_init_pwrlock(&pwrctrlpriv->check_32k_lock);
